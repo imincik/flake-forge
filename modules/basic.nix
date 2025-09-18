c44d780a512ec033698739bc4fa2c54912667433
Ivan Mincik <ivan.mincik@gmail.com>
2025-09-18 22:43:11 +0200

    Support multiple packages in default builder

--- modules/basic.nix
+++ modules/basic.nix
@@ -13,24 +13,36 @@
 
         packages = {
           builders = {
-            default = {
-              name = lib.mkOption {
-                type = lib.types.str;
-                default = "my-package";
-              };
-              version = lib.mkOption {
-                type = lib.types.str;
-                default = "1.0.0";
-              };
-              source = {
-                url = lib.mkOption {
-                  type = lib.types.str;
-                };
-                hash = lib.mkOption {
-                  type = lib.types.str;
-                  default = "";
-                };
-              };
+            default = lib.mkOption {
+              type = lib.types.listOf (
+                lib.types.submodule {
+                  options = {
+                    name = lib.mkOption {
+                      type = lib.types.str;
+                      default = "my-package";
+                    };
+                    version = lib.mkOption {
+                      type = lib.types.str;
+                      default = "1.0.0";
+                    };
+                    source = {
+                      url = lib.mkOption {
+                        type = lib.types.str;
+                      };
+                      hash = lib.mkOption {
+                        type = lib.types.str;
+                        default = "";
+                      };
+                    };
+                    requirements = {
+                      build = lib.mkOption {
+                        type = lib.types.listOf lib.types.package;
+                        default = [ ];
+                      };
+                    };
+                  };
+                }
+              );
             };
           };
 
@@ -44,29 +56,34 @@
 
       config.packages =
         let
-          builderDefault = pkgs.callPackage (
-            { stdenv, fetchurl }:
-            stdenv.mkDerivation {
-              pname = cfg.packages.builders.default.name;
-              version = cfg.packages.builders.default.version;
-              src = fetchurl {
-                url = cfg.packages.builders.default.source.url;
-                hash = cfg.packages.builders.default.source.hash;
-              };
-              buildInputs = [ ];
-            }
-          ) { };
+          builderDefault = lib.listToAttrs (
+            map (pkg: {
+              name = pkg.name;
+              value = pkgs.callPackage (
+                # Derivation start
+                { stdenv, fetchurl }:
+                stdenv.mkDerivation {
+                  pname = pkg.name;
+                  version = pkg.version;
+                  src = fetchurl {
+                    url = pkg.source.url;
+                    hash = pkg.source.hash;
+                  };
+                  buildInputs = pkg.requirements.build;
+                }
+                # Derivation end
+              ) { };
+            }) cfg.packages.builders.default
+          );
 
-          allPackages = lib.listToAttrs (
+          packagesNixpkgs = lib.listToAttrs (
             map (pkg: {
               name = pkg.pname;
-              value = pkg.overrideAttrs (prev: {
-                pname = prev.pname;
-              });
-            }) (cfg.packages.nixpkgs ++ [ builderDefault ])
+              value = pkg;
+            }) cfg.packages.nixpkgs
           );
         in
 
-        lib.mkIf cfg.enable allPackages;
+        lib.mkIf cfg.enable (packagesNixpkgs // builderDefault);
     };
 }
--- packages.nix
+++ packages.nix
@@ -1,6 +1,8 @@
 { inputs, ... }:
 
 {
+  debug = true;
+
   perSystem =
     {
       config,
@@ -15,13 +17,25 @@
 
         packages = {
           builders = {
-            default = {
-              name = "hello";
-              source = {
-                url = "mirror://gnu/hello/hello-2.12.1.tar.gz";
-                hash = "sha256-jZkUKv2SV28wsM18tCqNxoCZmLxdYH2Idh9RLibH2yA=";
-              };
-            };
+            default = [
+              {
+                name = "hello";
+                source = {
+                  url = "mirror://gnu/hello/hello-2.12.1.tar.gz";
+                  hash = "sha256-jZkUKv2SV28wsM18tCqNxoCZmLxdYH2Idh9RLibH2yA=";
+                };
+              }
+              {
+                name = "geos";
+                source = {
+                  url = "https://download.osgeo.org/geos/geos-3.9.6.tar.bz2";
+                  hash = "sha256-jChKNBWS+WDYSBPrujwv1PyesLZlggon9vi+shGrawA=";
+                };
+                requirements = {
+                  build = [ pkgs.cmake ];
+                };
+              }
+            ];
           };
 
           nixpkgs = [
