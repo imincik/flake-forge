280ba8a88942720be9eada5012d04b4d870424b5
Ivan Mincik <ivan.mincik@gmail.com>
2025-09-28 20:33:42 +0200

    dev(ui): update dev-help instructions

--- ui/develop.nix
+++ ui/develop.nix
@@ -27,14 +27,14 @@
               echo
               echo "'cd ui' first"
               echo
+              echo "Re-generate forge-config file:"
+              echo "  cat \$(nix build .#_forge-config --print-out-paths) | jq > src/forge-config.json"
+              echo
               echo "Launch Python web server:"
               echo "  python3 -m http.server &"
               echo
               echo "Re-build Elm app on change:"
               echo "  find src/ -name "*.elm" | entr -rn elm make src/Main.elm --output=src/main.js"
-              echo
-              echo "Re-generate forge-config file:"
-              echo "  cat \$(nix build .#_forge-config --print-out-paths) | jq > src/forge-config.json"
               echo
               echo "Run 'dev-help' to see this message again."
             }
