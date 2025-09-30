3f5f5a49a4a83f7331e57bb2054797c5c96f5e89
Ivan Mincik <ivan.mincik@gmail.com>
2025-09-30 19:02:20 +0200

    forge: move _forge-config package to forge/packages.nix

--- forge/packages.nix
+++ forge/packages.nix
@@ -13,14 +13,20 @@
 
     {
       packages = {
-        _forge-ui = pkgs.callPackage ../ui/package.nix {
-          inherit (config.packages) _forge-config;
+        _forge-config = pkgs.writeTextFile {
+          name = "forge-config.json";
+          text = builtins.toJSON config.forge;
         };
 
         _forge-packages = pkgs.symlinkJoin {
           name = "forge-packages";
           paths = lib.attrValues (lib.filterAttrs (n: v: !lib.hasPrefix "_forge" n) config.packages);
         };
+
+        _forge-ui = pkgs.callPackage ../ui/package.nix {
+          inherit (config.packages) _forge-config;
+        };
+
       };
     };
 }
