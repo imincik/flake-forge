d6facffa047134709e3095bd86f99c5054f93698
Ivan Mincik <ivan.mincik@gmail.com>
2025-10-07 13:20:14 +0200

    dev: run test for all packages containing test attribute

--- forge/packages.nix
+++ forge/packages.nix
@@ -16,11 +16,6 @@
           text = builtins.toJSON config.forge;
         };
 
-        _forge-packages = pkgs.symlinkJoin {
-          name = "forge-packages";
-          paths = lib.attrValues (lib.filterAttrs (n: v: !lib.hasPrefix "_forge" n) config.packages);
-        };
-
         _forge-ui = pkgs.callPackage ../ui/package.nix {
           inherit (config.packages) _forge-config;
         };
